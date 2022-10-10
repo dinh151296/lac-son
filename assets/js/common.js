d1bf29b8ec69c8d1c3e7012c2f42d427f97979c1
thanhdinh1996 <dinhturin1996@gmail.com>
2022-10-10 07:18:56 +0700

    new cm

---
+++ assets/js/common.js
@@ -0,0 +1,4 @@
+// handle click notification
+document.querySelector('.header__notification').addEventListener('click', function() {
+    document.querySelector('.notification__box').classList.toggle('active');
+});
